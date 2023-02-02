075665bd04036c5a55bacdd94491f85836570d7e
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-02-02 11:24:18 -0600

    add getURLsFromHTML

--- crawl.js
+++ crawl.js
@@ -1,3 +1,6 @@
+const { JSDOM } = require('jsdom')
+
+
 function normalizeURL(string) {
     if (string.slice(-1) === '/') {
         string = string.slice(0, -1)
@@ -7,6 +10,19 @@ function normalizeURL(string) {
     return urlObject.host.toLowerCase() + normalizedPathName
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+    let dom = new JSDOM(htmlBody)
+    let a_selector = dom.window.document.querySelectorAll('a')
+    let parsed_links = []
+    for (let a of a_selector) {
+        console.log(a.href)
+        parsed_links.push(a.href)
+    }
+    return parsed_links
+
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
   }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normalize url', () => {
@@ -8,4 +8,15 @@ test('normalize url', () => {
   expect(normalizeURL('http://wagslane.dev/path')).toBe('wagslane.dev/path');
   expect(normalizeURL('http://wagsLANE.DEV/PatH')).toBe('wagslane.dev/path');
 
+});
+
+const html1 = 
+`<html>
+<body>
+    <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+</body>
+</html>
+`
+test ('pull urls', () => {
+    expect(getURLsFromHTML(html1)).toStrictEqual(['https://blog.boot.dev/'])
 });
