932511590f60672b6044a49d219d8745f3d2cd91
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-02-02 11:31:36 -0600

    set up cli

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+
+
+function main(){
+    if (process.argv.length < 3) {
+        console.log('error: too few arguments')
+        return
+    } else if (process.argv.length > 3) {
+        console.log('error: too many arguments')
+        return
+    } else {
+        console.log('proceeding')
+        console.log('analyzing ' +  process.argv[2])
+    }
+  }
+  
+  main()
